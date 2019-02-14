4057153c7e8899fc003b0c48f3a5f22cd5e856f4
thuan <thua274@gmail.com>
2019-02-14 15:53:17 +0700

    profile

---
+++ admin/users/profile.php
@@ -0,0 +1,36 @@
+<?php  require_once $_SERVER['DOCUMENT_ROOT'].'/andy/templates/admin/inc/header.php'; ?>    
+
+
+    <section class="ftco-section contact-section">
+      <div class="container mt-5">
+        <div class="row block-9">
+			<div class="col-md-4 contact-info ftco-animate">
+				<div class="row">
+				<div class="col-md-12 mb-4">
+	        </div>
+			</div>
+			</div>
+					<div class="col-md-1"></div>
+          <div class="col-md-6 ftco-animate">
+            <form action="#" class="contact-form">
+            	<div class="row">
+            		<div class="col-md-6">
+	                <div class="form-group">
+	                  Name :
+	                </div>
+                </div>
+              </div>
+              <div class="form-group">
+                Email :
+              </div>
+              <div class="form-group">
+                Point :
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <div id="map"></div>
+<?php  require_once $_SERVER['DOCUMENT_ROOT'].'/andy/templates/admin/inc/footer.php'; ?> 
