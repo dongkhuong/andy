c7f544a48fae73f6d1711e50ca96b0d951a510d7
thuan <thua274@gmail.com>
2019-02-14 16:34:48 +0700

    show profile

--- admin/users/profile.php
+++ admin/users/profile.php
@@ -6,6 +6,7 @@
         <div class="row block-9">
 			<div class="col-md-4 contact-info ftco-animate">
 				<div class="row">
+                <img src=”..//admin/images/bg_1.jpg”  height=”50″ width=”50″ />
 				<div class="col-md-12 mb-4">
 	        </div>
 			</div>
@@ -15,8 +16,18 @@
             <form action="#" class="contact-form">
             	<div class="row">
             		<div class="col-md-6">
+                    <?php
+                      
+                      $user = "SELECT * FROM users WHERE id_user = 6";
+                      $rsuser= $mysqli->query($user);
+                      $row_user = $rsuser->fetch_assoc();
+                   
+                      
+                      
+                      ?>
 	                <div class="form-group">
-	                  Name :
+                      Name :<?php  echo $row_user['fullname']; ?>
+                     
 	                </div>
                 </div>
               </div>
